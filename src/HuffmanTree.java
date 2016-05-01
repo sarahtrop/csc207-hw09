e6dfb200120874a13d9f1be36bf39ab40609bf22
Sarah Trop <sctrop@gmail.com>
2016-05-01 16:37:16 -0500

    Working on building the HuffmanTree

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -8,7 +8,12 @@ public class HuffmanTree {
 	
 	public Node root;
 
-	// Combines two nodes and returns the new node
+	/**
+	 * Combines two nodes
+	 * @param l1	a Node
+	 * @param l2	a Node
+	 * @return		a Node
+	 */
 	public Node combine(Node l1, Node l2) {
 		return new Node(l1.v + l2.v, (short)0, l1, l2);
 	}
@@ -23,48 +28,74 @@ public class HuffmanTree {
 			// create a new node with the code and integer
 			queue.add(new Node(m.get(key), key, null, null));
 		}
-		// Now that we a have priority queue of nodes, link them to each other
-		for (int i = 0; i < m.size(); i++) {
-			Node childNode = getChild(queue); //get the lowest priority thing
-			if (root == null) { root = childNode; } // if the queue is null, make the root the new node
-			else if (root.left == null) { // if the left node is null, combine and add it to the left
-				root = combine(root, childNode);
-				root.left = childNode;
-			} else {  // if the right node is null, combine and add it to the right
-				root = combine(root, childNode);
-				root.right = childNode;
-			}
+		// Now that we a have priority queue of nodes, recursively combine nodes until we have a queue with 1 thing
+		root = buildTree(queue);
+	}
+	
+	/**
+	 * Method combines nodes from a PriorityQueue until there is only 1 left, a tree
+	 * @param queue	a PriorityQueue
+	 * @return		a Node
+	 */
+	public Node buildTree(PriorityQueue<Node> queue) {
+		if (queue.size() == 1) { // if there is only one thing left, return it 
+			return queue.poll();
+		} else { // otherwise, continue to combine nodes
+			Node lowest = next(queue); //get the lowest priority thing
+			Node nextLowest = next(queue); // get the next lowest priority thing
+			queue.add(combine(lowest, nextLowest));
+			return buildTree(queue);
 		}
 	}
 	
-	// Method created for Huffman Tree creation, gets the lowest priority thing and then removes it.
-	public Node getChild(PriorityQueue<Node> tree) {
-		Node temp = (Node) tree.toArray()[tree.size() - 1];
-		tree.remove(tree.size() - 1);
+	/**
+	 * Method gets the lowest priority item in a PriorityQueue
+	 * @param queue	a PriorityQueue
+	 * @return		a Node
+	 */
+	public Node next(PriorityQueue<Node> queue) {
+		Node temp = (Node) queue.toArray()[queue.size() - 1];
+		queue.remove(queue.size() - 1);
 		return temp;
 	}
 	
+	/**
+	 * Encodes a bit stream into a compressed representation
+	 * @param in	a BitInputStream
+	 * @param out	a BitOutputStream
+	 */
 	public void encode(BitInputStream in, BitOutputStream out) {
 		
 	}
 	
+	/**
+	 * Decodes a bit stream into a compressed representation
+	 * @param in	a BitInputStream
+	 * @param out	a BitOutputStream
+	 */
 	public void decode(BitInputStream in, BitOutputStream out) {
 		
 	}
 	
-	// Ascending order of values
-		private void inorderH(Node node) {
-			if (node == null) {
-				return;
-			}
-			inorderH(node.left);
-			System.out.print("[" + node.c + ", " + node.v + "]");
-			inorderH(node.right);
+	/**
+	 * Helper method to print a tree in order 	
+	 * @param node	a Node
+	 */
+	private void inorderH(Node node) {
+		if (node == null) {
+			return;
 		}
+		inorderH(node.left);
+		System.out.print("[" + node.c + ", " + node.v + "]");
+		inorderH(node.right);
+	}
 
-		public void inorderToString() {
-			inorderH(root);
-		}
+	/**
+	 * Calls helper method to print a tree in order
+	 */
+	public void treeToString() {
+		inorderH(root);
+	}
 	
 	public static void main(String[] args) {
 		Map<Short, Integer> m = new HashMap<>();
@@ -78,7 +109,7 @@ public class HuffmanTree {
 		
 		HuffmanTree t = new HuffmanTree(m);
 		
-		t.inorderToString();
+		t.treeToString();
 	}
 	
 }
